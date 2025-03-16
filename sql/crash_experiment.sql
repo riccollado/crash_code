c208f4fe68380fc6c1f19824177a811110675fcb
Ricardo Collado <collado.ricardo@gmail.com>
2025-03-16 05:48:05 -0400

    Refactor SQL table definitions and comments for consistency
    1. **sql/crash_meta_2.sql**: Reformatted table creation and comments for clarity.
    2. **sql/crash_output.sql**: Simplified table creation and updated comments.
    3. **sql/crash_meta.sql**: Improved formatting and comments for better readability.
    4. **sql/crash_output_2.sql**: Streamlined table definition and comments.
    5. **sql/crash_experiment_2.sql**: Enhanced formatting and comments for consistency.
    6. **sql/crash_iteration_2.sql**: Refined table structure and comments for clarity.
    7. **sql/crash_iteration.sql**: Updated formatting and comments for better understanding.

--- sql/crash_experiment.sql
+++ sql/crash_experiment.sql
@@ -1,19 +1,16 @@
 -- Table: public.crash_experiment
-
 -- DROP TABLE public.crash_experiment;
-
-CREATE TABLE public.crash_experiment
-(
-    id bigint NOT NULL DEFAULT nextval('crash_experiment_exp_id_seq'::regclass),
+CREATE TABLE public.crash_experiment (
+    id bigint NOT NULL DEFAULT nextval('crash_experiment_exp_id_seq' :: regclass),
     network jsonb NOT NULL,
     no_of_edges integer,
     no_of_nodes integer,
-    most_likely double precision[] NOT NULL,
-    optimistic double precision[] NOT NULL,
-    pessimistic double precision[] NOT NULL,
-    cov_mat double precision[] NOT NULL,
-    crash_cost double precision[] NOT NULL,
-    crash_time double precision[] NOT NULL,
+    most_likely double precision [] NOT NULL,
+    optimistic double precision [] NOT NULL,
+    pessimistic double precision [] NOT NULL,
+    cov_mat double precision [] NOT NULL,
+    crash_cost double precision [] NOT NULL,
+    crash_time double precision [] NOT NULL,
     penalty_b1 double precision NOT NULL,
     penalty_m double precision NOT NULL,
     penalty_steps integer NOT NULL,
@@ -35,21 +32,15 @@ CREATE TABLE public.crash_experiment
     seed_np integer,
     network_figure bytea,
     network_pos jsonb,
-    trial double precision[],
+    trial double precision [],
     CONSTRAINT crash_experiment_pkey PRIMARY KEY (id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+) WITH (OIDS = FALSE) TABLESPACE pg_default;
 
-ALTER TABLE public.crash_experiment
-    OWNER to ricardo;
-COMMENT ON TABLE public.crash_experiment
-    IS 'Main table holding individual crash experiments.';
+ALTER TABLE
+    public.crash_experiment OWNER to ricardo;
 
-COMMENT ON COLUMN public.crash_experiment.network_figure
-    IS 'Binary figure storage.';
+COMMENT ON TABLE public.crash_experiment IS 'Main table holding individual crash experiments.';
 
-COMMENT ON COLUMN public.crash_experiment.network_pos
-    IS 'Network figure node positions. It can be used for visualizations.';
+COMMENT ON COLUMN public.crash_experiment.network_figure IS 'Binary figure storage.';
+
+COMMENT ON COLUMN public.crash_experiment.network_pos IS 'Network figure node positions. It can be used for visualizations.';
--- sql/crash_experiment_2.sql
+++ sql/crash_experiment_2.sql
@@ -1,15 +1,14 @@
-CREATE TABLE public.crash_experiment
-(
+CREATE TABLE public.crash_experiment (
     id bigserial,
     network jsonb NOT NULL,
     no_of_edges integer,
     no_of_nodes integer,
-    most_likely double precision[] NOT NULL,
-    optimistic double precision[] NOT NULL,
-    pessimistic double precision[] NOT NULL,
+    most_likely double precision [] NOT NULL,
+    optimistic double precision [] NOT NULL,
+    pessimistic double precision [] NOT NULL,
     cov_mat jsonb NOT NULL,
-    crash_cost double precision[] NOT NULL,
-    crash_time double precision[] NOT NULL,
+    crash_cost double precision [] NOT NULL,
+    crash_time double precision [] NOT NULL,
     penalty_b1 double precision NOT NULL,
     penalty_m double precision NOT NULL,
     penalty_steps integer NOT NULL,
@@ -32,18 +31,13 @@ CREATE TABLE public.crash_experiment
     network_figure bytea,
     network_pos jsonb,
     CONSTRAINT experiment_id PRIMARY KEY (id)
-)
-WITH (
-    OIDS = FALSE
-);
+) WITH (OIDS = FALSE);
 
-ALTER TABLE public.crash_experiment
-    OWNER to ricardo;
-COMMENT ON TABLE public.crash_experiment
-    IS 'Main table holding individual crash experiments.';
+ALTER TABLE
+    public.crash_experiment OWNER to ricardo;
 
-COMMENT ON COLUMN public.crash_experiment.network_figure
-    IS 'Binary figure storage.';
+COMMENT ON TABLE public.crash_experiment IS 'Main table holding individual crash experiments.';
 
-COMMENT ON COLUMN public.crash_experiment.network_pos
-    IS 'Network figure node positions. It can be used for visualizations.';
+COMMENT ON COLUMN public.crash_experiment.network_figure IS 'Binary figure storage.';
+
+COMMENT ON COLUMN public.crash_experiment.network_pos IS 'Network figure node positions. It can be used for visualizations.';
