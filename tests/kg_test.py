9d59f1e9830ad6e453533250a90e82279017c981
Ricardo Collado <collado.ricardo@gmail.com>
2025-03-17 06:42:09 -0400

    Add tests for Knowledge Gradient methods in kg_test.py
    1. **tests/kg_test.py**: Implemented tests for KG_Alg, KG_multi, and update_mu_S functions using a random correlation matrix.

---
+++ tests/kg_test.py
@@ -0,0 +1,68 @@
+"""
+A simple test of the Knowledge Gradient (KG) method.
+
+This script tests the functionality of the KG_Alg, KG_multi, and update_mu_S functions
+from the opt_manager.knowledge_gradient module. It generates a random correlation
+matrix, adjusts it to be the nearest correlation matrix, and then applies the KG methods
+to update the mean vector and covariance matrix based on the given parameters.
+
+Functions tested:
+- KG_Alg: Computes the Knowledge Gradient for a single decision.
+- KG_multi: Computes the Knowledge Gradient for multiple decisions using
+multiprocessing.
+- update_mu_S: Updates the mean vector and covariance matrix after observing a new data
+point.
+"""
+
+import multiprocessing as mp
+
+import numpy as np
+from scipy.stats import random_correlation
+
+from matrix_manager.nearest_correlation import nearcorr
+from opt_manager.knowledge_gradient import KG_Alg, KG_multi, update_mu_S
+
+if __name__ == "__main__":
+    processes = mp.cpu_count()
+    pool = mp.Pool(processes)
+
+    np.random.seed(126)
+
+    g = 7 / sum([0.5, 0.8, 1.2, 2.5, 1.7, 2.1, 2.2])
+    G = np.round(
+        random_correlation.rvs(
+            (g * 0.5, g * 0.8, g * 1.2, g * 2.5, g * 1.7, g * 2.1, g * 2.2)
+        ),
+        3,
+    )
+
+    S = nearcorr(
+        G,
+        tol=[],
+        flag=0,
+        max_iterations=1000,
+        weights=None,
+        except_on_too_many_iterations=True,
+    )
+
+    M = S.shape[0]
+
+    lambda_values = np.array([0.2, 1.1, 1.3, 0.12, 0.4, 0.3, 0.12])
+
+    mu = np.array([0.2, 0.21, 0.92, 0.11, 0.7, 0.2, -0.1])
+
+    print(KG_Alg(mu, S, lambda_values))
+
+    print(KG_multi(mu, S, lambda_values, pool))
+
+    Y = 0.22
+    X = 3
+    updated_mu, updated_S = update_mu_S(mu, S, lambda_values, X, Y)
+    print(updated_mu.shape)
+    print(updated_mu)
+
+    print(updated_S.shape)
+    print(updated_S)
+
+    pool.close()
+    pool.join()
